bfc7a63e21e08523f4652bf525eb0e2732ec7bd0
Abdallah Bagato <95651979+AbdallahBagato@users.noreply.github.com>
2025-02-28 23:06:13 +0200

    main.py

--- project/main.py
+++ project/main.py
@@ -62,6 +62,6 @@ def search():
 if __name__=="__main__":
     with app.app_context():
         db.create_all()
-    app.run(port=8000,debug=True)
+    app.run(host='0.0.0.0',port=8000,debug=True)
 
 
