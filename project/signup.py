4bab52549b757bc54b0b879f8562036bd237b265
Abdallah Bagato <95651979+AbdallahBagato@users.noreply.github.com>
2025-07-24 02:11:18 +0300

    Update signup.py

--- project/signup.py
+++ project/signup.py
@@ -1,20 +1,47 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField,StringField,PasswordField,EmailField,DateField
-from wtforms.validators import DataRequired,length,Email,EqualTo
+from wtforms import SubmitField, StringField, PasswordField, EmailField, DateField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class Signup(FlaskForm):
-    
-    username= StringField('username',validators=[DataRequired(),length(max=20)])
-    email=EmailField("Email",validators=[DataRequired()])
-    password= PasswordField('password',validators=[DataRequired(),length(max=20,min=3)])
-    Re_password= PasswordField('confirm_password',validators=[DataRequired(),EqualTo("password",message='password didnt match')])
-    Fname= StringField('First name',validators=[DataRequired(),length(max=10)])
-    Lname= StringField('Last name',validators=[DataRequired(),length(max=10)])
-    NID = StringField('National ID',validators=[DataRequired()])
-    BD = DateField('BirthDate',format='%Y-%m-%d')
-    submit= SubmitField('submit')
+    username = StringField(
+        'username',
+        validators=[DataRequired(), Length(max=20)]
+    )
+    email = EmailField(
+        "Email",
+        validators=[DataRequired(), Email()]
+    )
+    password = PasswordField(
+        'password',
+        validators=[DataRequired(), Length(max=20, min=3)]
+    )
+    Re_password = PasswordField(
+        'confirm_password',
+        validators=[
+            DataRequired(),
+            EqualTo("password", message='password didnt match')
+        ]
+    )
+    Fname = StringField(
+        'First name',
+        validators=[DataRequired(), Length(max=10)]
+    )
+    Lname = StringField(
+        'Last name',
+        validators=[DataRequired(), Length(max=10)]
+    )
+    NID = StringField(
+        'National ID',
+        validators=[DataRequired()]
+    )
+    BD = DateField('BirthDate', format='%Y-%m-%d')
+    submit = SubmitField('submit')
+
 
 class Search(FlaskForm):
-    NID = StringField('National ID',validators=[DataRequired()])
-    submit= SubmitField('submit')
-
+    NID = StringField(
+        'National ID',
+        validators=[DataRequired()]
+    )
+    submit = SubmitField('submit')
